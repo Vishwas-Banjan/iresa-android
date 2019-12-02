b2523266b6827d25dc4db9a06013cb3fb8f8f9a7
Vishwas Banjan <vishwasbanjan@gmail.com>
2019-12-01 22:01:57 -0500

    Updated request Permisson, Swipe functioning, and SVG images - Issue with scaling SVG

--- Iresa/app/src/main/java/com/vbanjan/iresa/IntroActivity.java
+++ Iresa/app/src/main/java/com/vbanjan/iresa/IntroActivity.java
@@ -2,6 +2,8 @@ package com.vbanjan.iresa;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
@@ -9,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.github.paolorotolo.appintro.AppIntro;
@@ -24,14 +27,36 @@ public class IntroActivity extends AppIntro2 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        askForPermissions(new String[]{Manifest.permission.CAMERA}, 2);
-
         setUpIntro();
+    }
+
+    public void askPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "App requires camera to function properly", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_CAMERA_REQUEST_CODE);
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_CAMERA_REQUEST_CODE);
+            }
+        } else {
+            finish();
+        }
     }
 
     public void setUpIntro() {
         showSkipButton(false);
         setBarColor(Color.GRAY);
+        setVibrate(true);
+        setVibrateIntensity(30);
         addSlide(new WalkThroughFragment01()); //We're here/ at restaurant/bar
         addSlide(new WalkThroughFragment02()); //Scan the magic code
         addSlide(new WalkThroughFragment03()); //vote for songs
@@ -39,18 +64,17 @@ public class IntroActivity extends AppIntro2 {
 
     @Override
     public void onBackPressed() {
-        Toast.makeText(this, "Please check out what we can do!", Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onDonePressed(Fragment currentFragment) {
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "Camera permission is required for the app to function", Toast.LENGTH_SHORT).show();
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
-        } else {
-            finish();
-        }
+        askPermission();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
